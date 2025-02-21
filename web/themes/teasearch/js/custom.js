4a41cfe2ba135cd0883394f3299f61d4f39970d2
Alessandro Mantelli <alessandro.mantelli@unive.it>
2025-02-22 00:30:11 +0100

    first commit

---
+++ web/themes/teasearch/js/custom.js
@@ -0,0 +1,7 @@
+(function ($) {
+  // Your jQuery code using $ alias goes here.
+  $(document).ready(function () {
+    console.log("The $ alias is available within this scope.");
+  });
+})(jQuery);
+
